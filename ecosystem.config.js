1bafc16ccef9d2b1fd812aae482b03267db5661a
johanpoyet <jojopoy2003@gmail.com>
2025-12-21 10:58:30 +0100

    modif ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,14 @@
 module.exports = {
-  apps: [
-    {
-      name: 'planify',
-      script: 'npm',
-      args: 'start',
-      cwd: '/home/johan/apps/planify',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3000
-      }
+  apps: [{
+    name: 'planify',
+    script: 'npm',
+    args: 'start',
+    cwd: '/home/johan/apps/planify',
+    instances: 1,
+    exec_mode: 'fork',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
     }
-  ]
-};
+  }]
+}
